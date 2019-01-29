7b2a177ab297ed93450e45b7ac0d2cf891520208
Jose Pagan <jm.pagan@gmail.com>
2019-01-29 14:16:36 +0100

    added cpu game timings

--- src/App.js
+++ src/App.js
@@ -8,8 +8,13 @@ class App extends Component {
     this.handleClick = this.handleClick.bind(this)
     this.state = {
       targetSeq: ['red', 'yellow','blue','yellow'],
-     step: 0 
+      step: 0,
+      playerTime: false
     }
+  }
+
+  componentDidMount(){
+    this.gameLoop()
   }
 
   genColor(){
@@ -17,42 +22,70 @@ class App extends Component {
   const magicNumber = Math.floor(Math.random() * colors.length)
   return colors[magicNumber]
   }
+
   rainbowArray(n){
     const colorfulArray = [] 
     for (let a=0; a < n; a++){
     colorfulArray.push(this.genColor())
   }
-    
       return colorfulArray
   }
   
-  handleClick(event){ 
-    // const targetPad = this.state.targetSeq[this.state.step]  
-    // if (event !== targetPad)  {
-    //   console.log("you lose!!!")
-    // }    
-    console.log(this.state.step, this.state.targetSeq.length)
-    if (this.state.step === this.state.targetSeq.length -1) {
-console.log("last step passed?")
-      
-      const newTargetSeq = this.rainbowArray(this.state.targetSeq.length + 1 )
-      this.setState({
-        targetSeq: newTargetSeq,
-        step: 0
+  handleClick(event){
+       
+      // 
+      // to-do wrong note handling...
+      //
+      //if the last note of the simon game has been played...
+      if (this.state.step === this.state.targetSeq.length -1) {
+        //set new state with new sequence of colours (obviosly with one note more)
+        const newTargetSeq = this.rainbowArray(this.state.targetSeq.length + 1 )
+        this.setState({
+          targetSeq: newTargetSeq,
+          step: 0,
+          playerTime: false
+        })
+        this.playSeq()
+        //otherwise... carry on with next step
+      } else {
+        this.setState((prevState)=>{
+          return {
+            step: prevState.step + 1
+          }
+        })
+      }
+      }
 
-      })
-    } else {
-      this.setState((prevState)=>{
-        return {
-          step: prevState.step + 1
-        }
-      })
-    }
+  playSeq(){
+    this.playing = setInterval(()=>{
+      if (this.state.step !== this.state.targetSeq.length ){
+        console.log(this.state.targetSeq[this.state.step],this.state.step)
+        this.setState((prevState)=>{
+          return {
+            step: prevState.step + 1
+          }
+        })
+      } else {
+        clearInterval(this.playing)
+        this.setState({playerTime:true,
+          step:0
+        })
+      }
+
+    },500)
+  }
+
+
+  gameLoop(){
+    this.playSeq() 
 
   }
+
   render() {
-    // console.log(this.state.targetSeq)
-    // console.log(this.state.step)
+
+
+    console.log(this.state.targetSeq)
+    console.log(this.state.step)
     return (
       <div className="App">
         <Pad ofColour='green' clickProp={this.handleClick}/> 
